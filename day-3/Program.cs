76d0446cb150015658d47449ca877433bd03264c
megalon <megalongaming@gmail.com>
2025-02-07 16:29:43 -0500

    Copy relevant parts from data into a buffer

--- day-3/Program.cs
+++ day-3/Program.cs
@@ -11,6 +11,8 @@ namespace aoc_2024_day_3
 
             string data = File.ReadAllText(filePath);
 
+            string processedData = ProcessData(data);
+
             // get all the matches in a List using regex
             // regex something like "mul("[0-9]*","[0-9]*")"
             string regex = @"mul\([0-9]*,[0-9]*\)";
@@ -20,7 +22,7 @@ namespace aoc_2024_day_3
             long total = 0;
 
             // iterate through list
-            foreach(Match match in Regex.Matches(data, regex))
+            foreach(Match match in Regex.Matches(processedData, regex))
             {
                 int indexOfComma = match.Value.IndexOf(',');
 
@@ -36,6 +38,55 @@ namespace aoc_2024_day_3
             }
 
             Console.WriteLine(total);
+        }
+
+        /// <summary>
+        /// This uses a buffer to read through all of the input data and track the current "don't()" and "do()" pairs
+        /// When a pair is found, the characters before it are added to the processedData string, and the characters inside the pair is ignored
+        /// </summary>
+        private static string ProcessData(string data)
+        {
+            string processedData = "";
+
+            int bufferSize = "don't()".Length;
+            char[] buffer = new char[bufferSize];
+            string bufferString = "";
+            char c = ' ';
+            int processIndex = 0, dontIndex = 0;
+
+            for (int i = 0; i < data.Length; ++i)
+            {
+                c = data[i];
+
+                // 2 bytes per char
+                Buffer.BlockCopy(buffer, 2, buffer, 0, (bufferSize - 1) * 2);
+
+                buffer[bufferSize - 1] = c;
+
+                bufferString = new string(buffer);
+
+                if (bufferString.Contains("don't()"))
+                {
+                    dontIndex = i;
+                }
+                else if (bufferString.Contains("do()"))
+                {
+                    // substring from previous position to current "don't()" position
+                    processedData += data.Substring(processIndex, dontIndex - processIndex);
+
+                    // move process index to current read index to skip the chars between don't and do
+                    processIndex = i;
+                    dontIndex = i;
+
+                    // clear the buffer so we don't trigger the condition again on the next iteration
+                    buffer = new char[bufferSize];
+                }
+            }
+
+            // Special case for end of file
+            processedData += data.Substring(processIndex);
+
+            return processedData;
         }
     }
 }
