695c2ee251ef75d28aa189433eca968a25431069
megalon <megalongaming@gmail.com>
2025-02-07 11:16:13 -0500

    day 2 notes and psudocode

--- day-2/Program.cs
+++ day-2/Program.cs
@@ -5,6 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // read in file
+            {
+                // parse all numbers in line into a list
+
+                // iterate through list
+                {
+                    // keep track of difference for each step
+                }
+
+                // if report is safe
+                {
+                    // increment safe counter
+                }
+            }
+
+            // log result
         }
     }
 }
