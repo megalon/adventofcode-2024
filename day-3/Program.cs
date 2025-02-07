d8378beb5aba90773f807164e908c29fe04f24ab
megalon <megalongaming@gmail.com>
2025-02-07 13:00:52 -0500

    Read in data from file

--- day-3/Program.cs
+++ day-3/Program.cs
@@ -6,6 +6,13 @@
         {
             // read in data
 
+            string filename = "data.txt";
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
+
+            string data = File.ReadAllText(filePath);
+
+            Console.WriteLine(data);
+
             // get all the matches in a List using regex
             // regex something like "mul("[0-9]*","[0-9]*")"
 
