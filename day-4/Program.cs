f0f95282d67d85b038eebf9617773d29301734f2
megalon <megalongaming@gmail.com>
2025-02-07 17:15:08 -0500

    notes and psudocode

--- day-4/Program.cs
+++ day-4/Program.cs
@@ -5,6 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // iterate through every character in data
+            {
+                // check if character is an X
+
+                // right
+
+                // left
+
+                // up
+
+                // down
+
+                // diag up right
+
+                // diag up left
+
+                // diag down right
+
+                // diag down left
+            }
+
+            // log total
         }
     }
 }
