318672b5744b6a85437398aba0e86e98a9a3404e
megalon <megalongaming@gmail.com>
2025-02-07 17:54:13 -0500

    Read text into a character matrix and implement find right

--- day-4/Program.cs
+++ day-4/Program.cs
@@ -10,34 +10,68 @@ namespace aoc_2024_day_4
 
             string text = File.ReadAllText(filepath);
 
-            int width = text.Substring(0, text.IndexOf('\n')).Trim().Length;
-            int height = Regex.Count(text, @"\n") + 1;
+            string[] lines = text.Split('\n');
 
-            Console.WriteLine(width + " " +  height);
+            char[,] matrix = new char[lines[0].Length, lines.Length];
+
+            for (int x = 0; x < lines.Length - 1; ++x)
+            {
+                for (int y = 0; y < lines[0].Length - 1; ++y)
+                {
+                    matrix[x, y] = lines[x][y];
+                }
+            }
+
+            int total = 0;
+            string textToFind = "XMAS";
 
             // iterate through every character in data
-            foreach (char c in text)
+            for (int x = 0; x < matrix.GetLength(0); ++x)
             {
-                // check if character is an X
+                for (int y = 0; y < matrix.GetLength(1); ++y)
+                {
+                    char c = matrix[x, y];
 
-                // right
+                    Console.Write(c);
 
-                // left
+                    // check if character is an X
+                    if (c != 'X') continue;
 
-                // up
+                    total += FindRight(matrix, x, y, textToFind);
 
-                // down
+                    // left
 
-                // diag up right
+                    // up
 
-                // diag up left
+                    // down
 
-                // diag down right
+                    // diag up right
 
-                // diag down left
+                    // diag up left
+
+                    // diag down right
+
+                    // diag down left
+
+                }
+                
+                Console.WriteLine();
             }
 
             // log total
+            Console.WriteLine(total);
+        }
+
+        private static int FindRight(char[,] matrix,  int x, int y, string textToFind)
+        {
+            string foundText = "X";
+
+            for (int i = 1; i < matrix.GetLength(0) && i < textToFind.Length; ++i)
+            {
+                foundText += matrix[x + i, y];
+            }
+
+            return foundText.Equals(textToFind) ? 1 : 0;
         }
     }
 }
