35c3bf30c0cfeafc70b2922292e25485ea5466e2
megalon <megalongaming@gmail.com>
2025-02-07 11:35:30 -0500

    Parse reports and track safe reports that have a distance of 1 or 3

--- day-2/Program.cs
+++ day-2/Program.cs
@@ -9,23 +9,45 @@
             string filename = "data.txt";
             string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename);
 
+            long totalSafeReports = 0;
+
             foreach (string line in File.ReadLines(filePath))
             {
+                //Console.WriteLine(line);
+
                 // parse all numbers in line into a list
-                Console.WriteLine(line);
+                List<int> levelsList = new List<int>();
+                
+                foreach (string num in line.Split())
+                {
+                    levelsList.Add(int.Parse(num));
+                }
+
+                bool isSafe = true;
+                int prevDistance = 0;
 
                 // iterate through list
+                for (int i = 1; i < levelsList.Count; ++i)
                 {
-                    // keep track of difference for each step
+                    int distance = levelsList[i] - levelsList[i - 1];
+
+                    if (Math.Abs(distance) < 1 || Math.Abs(distance) > 3)
+                    {
+                        isSafe = false;
+                        break;
+                    }
                 }
 
                 // if report is safe
+                if (isSafe)
                 {
                     // increment safe counter
+                    ++totalSafeReports;
                 }
             }
 
             // log result
+            Console.WriteLine(totalSafeReports);
         }
     }
 }
