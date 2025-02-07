a5d1945f6abd0fd79596cc1f0184754d8f695adb
megalon <megalongaming@gmail.com>
2025-02-07 17:22:06 -0500

    Read in data and get width and height of matrix

--- day-4/Program.cs
+++ day-4/Program.cs
@@ -1,12 +1,22 @@
-﻿namespace aoc_2024_day_4
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_2024_day_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
+
+            string text = File.ReadAllText(filepath);
+
+            int width = text.Substring(0, text.IndexOf('\n')).Trim().Length;
+            int height = Regex.Count(text, @"\n") + 1;
+
+            Console.WriteLine(width + " " +  height);
 
             // iterate through every character in data
+            foreach (char c in text)
             {
                 // check if character is an X
 
