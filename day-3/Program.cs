836b9deacfed863922cc5fb583df92d0c68172db
megalon <megalongaming@gmail.com>
2025-02-07 12:58:28 -0500

    Day 3 notes and psudocode

--- day-3/Program.cs
+++ day-3/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // read in data
+
+            // get all the matches in a List using regex
+            // regex something like "mul("[0-9]*","[0-9]*")"
+
+            // iterate through list
+            {
+                // get 1st number (a)
+
+                // get 2nd number (b)
+
+                // total += a * b
+            }
+
+            // log total
         }
     }
 }
