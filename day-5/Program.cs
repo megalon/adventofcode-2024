df2d044962628c144cf143c7b6aba62e002961d4
megalon <megalongaming@gmail.com>
2025-02-08 10:15:38 -0500

    Check validity of lists using rules dict

--- day-5/Program.cs
+++ day-5/Program.cs
@@ -18,7 +18,7 @@ namespace aoc_2024_day_5
             // Build dictionary using these rules
             foreach (Match match in Regex.Matches(text, @"([0-9]+)\|([0-9]+)"))
             {
-                Console.WriteLine(match.Groups[1].Value + " " + match.Groups[2].Value);
+                //Console.WriteLine(match.Groups[1].Value + " " + match.Groups[2].Value);
 
                 int page1 = int.Parse(match.Groups[1].Value);
                 int page2 = int.Parse(match.Groups[2].Value);
@@ -33,25 +33,45 @@ namespace aoc_2024_day_5
             }
 
             // Collect all lists of pages
-            // Convert strings to list of ints
             foreach (Match match in Regex.Matches(text, @"([0-9]+,)+[0-9]+([\r\n]+|$)"))
             {
-                Console.WriteLine(match.Value.Trim());
-            }
-
-            // Iterate through lists
-            {
-                // valid = true
-
-                // Iterate through each element in list
+                // Convert strings to array of ints
+                string[] pagesStringArray = match.Value.Trim().Split(',');
+                int[] pagesArray = new int[pagesStringArray.Length];
+                for (int i = 0; i < pagesStringArray.Length; ++i)
                 {
-                    // Check element against all other elements
-                    // Using rules from rule dict
-
-                    // If failure, valid = false, break
+                    pagesArray[i] = int.Parse(pagesStringArray[i]);
                 }
 
-                // if !valid: continue
+                bool valid = true;
+
+                // Iterate 
+                for (int i = pagesArray.Length - 1; i >= 0; --i)
+                {
+                    int page = pagesArray[i];
+
+                    // Check element against all preceding elements
+                    for (int j = i - 1; j >= 0; --j)
+                    {
+                        // If the rules dictionary for this page
+                        // contains a rule one of the elements before this in the list
+                        // then this page is in the wrong order
+                        if (rulesDict.TryGetValue(page, out List<int> list))
+                        {
+                            if (list.Contains(pagesArray[j]))
+                            {
+                                valid = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!valid) break;
+                }
+                
+                Console.WriteLine($"{match.Value.Trim()} -> {(valid ? "VALID" : "INVALID")}");
+
+                if (!valid) continue;
 
                 // find middle value
 
