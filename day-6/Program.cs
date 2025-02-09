47664ad2b8ff6f4385f37b88025dc2fba52df0bc
megalon <megalongaming@gmail.com>
2025-02-09 12:32:53 -0500

    Refactor to track collisions

--- day-6/Program.cs
+++ day-6/Program.cs
@@ -12,7 +12,6 @@ namespace aoc_2024_day_6
             char[,] matrix = new char[lines[0].Length, lines.Length];
 
             Vector2Int guardPosition = new Vector2Int(0, 0);
-            MovementChoice currentMovement = MovementChoice.UP;
 
             // Build the char matrix
             for (int y = 0; y < matrix.GetLength(1); ++y)
@@ -28,10 +27,15 @@ namespace aoc_2024_day_6
                 //Console.WriteLine();
             }
 
-            while (currentMovement != MovementChoice.FINISH)
+            CollisionPoint collisionPoint = Move(matrix, ref guardPosition, MovementChoice.UP);
+
+            while (!collisionPoint.isOutOfBounds)
             {
-                currentMovement = Move(matrix, ref guardPosition, currentMovement);
+                Console.WriteLine($"Collided at ({collisionPoint.pos.x}, {collisionPoint.pos.y}) moving {collisionPoint.movementWhenHit.ToString()}");
+                collisionPoint = Move(matrix, ref guardPosition, collisionPoint.nextMove);
             }
+
+            Console.WriteLine($"Finished at ({collisionPoint.pos.x}, {collisionPoint.pos.y}) moving {collisionPoint.movementWhenHit.ToString()}");
 
             int total = 0;
             foreach (char c  in matrix)
@@ -42,19 +46,19 @@ namespace aoc_2024_day_6
             Console.WriteLine(total);
         }
 
-        private static MovementChoice Move(char[,] matrix, ref Vector2Int guardPosition, MovementChoice direction)
+        /// <returns>Collision point</returns>
+        private static CollisionPoint Move(char[,] matrix, ref Vector2Int guardPosition, MovementChoice direction)
         {
             Vector2Int delta = new Vector2Int(0, 0);
 
-            MovementChoice nextMove;
+            CollisionPoint collisionPoint;
 
             switch (direction)
             {
-                case MovementChoice.UP: delta = new Vector2Int(0, -1); nextMove = MovementChoice.RIGHT; break;
-                case MovementChoice.DOWN: delta = new Vector2Int(0, 1); nextMove = MovementChoice.LEFT; break;
-                case MovementChoice.LEFT: delta = new Vector2Int(-1, 0); nextMove = MovementChoice.UP; break;
-                case MovementChoice.RIGHT: delta = new Vector2Int(1, 0); nextMove = MovementChoice.DOWN; break;
-                default: return MovementChoice.FINISH;
+                case MovementChoice.UP: delta = new Vector2Int(0, -1); break;
+                case MovementChoice.DOWN: delta = new Vector2Int(0, 1); break;
+                case MovementChoice.LEFT: delta = new Vector2Int(-1, 0); break;
+                case MovementChoice.RIGHT: delta = new Vector2Int(1, 0); break;
             }
 
             matrix[guardPosition.x, guardPosition.y] = 'X';
@@ -65,17 +69,20 @@ namespace aoc_2024_day_6
             {
                 nextPosition = guardPosition + delta;
 
+                // If we went out of bounds
                 if (nextPosition.x < 0
                  || nextPosition.y < 0
                  || nextPosition.x > matrix.GetLength(0) - 1
                  || nextPosition.y > matrix.GetLength(1) - 1)
                 {
-                    return MovementChoice.FINISH;
+                    collisionPoint = new CollisionPoint(guardPosition, direction, true);
+                    break;
                 }
 
                 // If we hit an obstacle
                 if (matrix[nextPosition.x, nextPosition.y] == '#')
                 {
+                    collisionPoint = new CollisionPoint(guardPosition, direction, false);
                     break;
                 }
 
@@ -94,7 +101,7 @@ namespace aoc_2024_day_6
                 //Console.WriteLine();
             }
 
-            return nextMove;
+            return collisionPoint;
         }
 
         private struct Vector2Int
@@ -112,13 +119,38 @@ namespace aoc_2024_day_6
             public static Vector2Int operator +(Vector2Int a, Vector2Int b) => new Vector2Int(a.x + b.x, a.y + b.y);
         }
 
+        private struct CollisionPoint
+        {
+            public Vector2Int pos { get; }
+            public MovementChoice movementWhenHit { get; }
+            public MovementChoice nextMove { get { return CalculateNextMove(); } }
+            public bool isOutOfBounds { get; }
+
+            public CollisionPoint(Vector2Int pos, MovementChoice movementWhenHit, bool isOutOfBounds)
+            {
+                this.pos = pos;
+                this.movementWhenHit = movementWhenHit;
+                this.isOutOfBounds = isOutOfBounds;
+            }
+
+            private MovementChoice CalculateNextMove()
+            {
+                switch (movementWhenHit)
+                {
+                    case MovementChoice.DOWN: return MovementChoice.LEFT;
+                    case MovementChoice.LEFT: return MovementChoice.UP;
+                    case MovementChoice.RIGHT: return MovementChoice.DOWN;
+                    default: return MovementChoice.RIGHT;
+                }
+            }
+        }
+
         private enum MovementChoice
         {
             UP,
             DOWN,
             LEFT,
-            RIGHT,
-            FINISH
+            RIGHT
         }
     }
 }
