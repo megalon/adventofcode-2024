c12d9c575a8022157b7014369ed096ca2dd8b89d
megalon <megalongaming@gmail.com>
2025-02-08 21:15:55 -0500

    Read in data into a char matrix

--- day-6/Program.cs
+++ day-6/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
+            string[] lines = File.ReadAllLines(filepath);
+
+            char[,] matrix = new char[lines[0].Length, lines.Length];
+
+            for (int y = 0; y < matrix.GetLength(1); ++y)
+            {
+                for (int x = 0; x < matrix.GetLength(0); ++x)
+                {
+                    matrix[x, y] = lines[y][x];
+
+                    Console.Write(matrix[x, y]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
