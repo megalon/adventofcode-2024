49378effb03449ac89cbd1c7e04d840fba36513d
megalon <megalongaming@gmail.com>
2025-02-11 11:37:21 -0500

    Use recursion to calculate options

--- day-7/Program.cs
+++ day-7/Program.cs
@@ -10,12 +10,32 @@ namespace aoc_2024_day_7
 
             string[] text = File.ReadAllLines(filepath);
 
+            int numValid = 0;
+
             foreach (string line in text) {
                 Console.WriteLine(line);
 
                 int target = int.Parse(line.Substring(0, line.IndexOf(':')));
                 int[] values = line.Substring(line.IndexOf(": ") + 2).Split(' ').Select(int.Parse).ToArray();
+
+                if (DoesCalculate(values, 0, target, 0))
+                {
+                    ++numValid;
+                }
             }
+
+            Console.WriteLine(numValid);
+        }
+
+        private static bool DoesCalculate(int[] values, int total, int target, int index)
+        {
+            if (index == values.Length)
+            {
+                return total == target;
+            }
+
+            return DoesCalculate(values, total + values[index], target, index + 1)
+                || DoesCalculate(values, total * values[index], target, index + 1);
         }
     }
 }
