17b65f6ffd0de570dbebe689b49f01dcd4fdb995
megalon <megalongaming@gmail.com>
2025-02-12 19:29:01 -0500

    Notes and pseudocode

--- day-8/Program.cs
+++ day-8/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // Read in data
+
+            // Iterate through data and create a dictionary containing all the antenna locations
+            {
+                
+            }
+
+            // Iterate through antenna locations dictionary
+            {
+                // Calculate anode locations based on pairs
+
+                // Count pairs and add to total
+            }
+
+            // Display total
         }
     }
 }
