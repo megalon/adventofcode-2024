6f5101ac1e17232aa1419afc170724583b4b99b5
megalon <megalongaming@gmail.com>
2025-02-12 19:34:54 -0500

    Parse out the antennas from the input data and test counting them

--- day-8/Program.cs
+++ day-8/Program.cs
@@ -8,9 +8,29 @@
             string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
             string[] data = File.ReadAllLines(filepath);
 
+            Dictionary<char, int> antennaMap = new Dictionary<char, int>();
+
             // Iterate through data and create a dictionary containing all the antenna locations
             foreach (string line in data) {
                 Console.WriteLine(line);
+
+                foreach (char c in line)
+                {
+                    if (c == '.') continue;
+
+                    if (antennaMap.ContainsKey(c))
+                    {
+                        antennaMap[c] += 1;
+                    } else
+                    {
+                        antennaMap[c] = 1;
+                    }
+                }
+            }
+
+            foreach (char c in  antennaMap.Keys)
+            {
+                Console.WriteLine($"{c} : {antennaMap[c]}");
             }
 
             // Iterate through antenna locations dictionary
