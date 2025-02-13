10a12e52eca9ebb954edfcab034036beb1f8fb20
megalon <megalongaming@gmail.com>
2025-02-13 11:54:27 -0500

    Pseudocode and read in data from file

--- day-9/Program.cs
+++ day-9/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // read in data
+            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
+            string data = File.ReadAllText(filepath);
+
+            Console.WriteLine(data);
+
+            // parse diskmap into a string
+            {
+                // diskmap is a string of pairs of digits
+                // 1st digit is file length
+                // 2nd digit is free space
+                // 12 = file length 1, free space 2 -> "0.."
+            }
+
+            // iterate through string
+            {
+                // take char from end and place in empty space at front
+            }
         }
     }
 }
