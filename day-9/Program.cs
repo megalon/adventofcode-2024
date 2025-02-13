7799ba924855412226a1976e0f2db2ca099a0a12
megalon <megalongaming@gmail.com>
2025-02-13 12:10:54 -0500

    Parse diskmap from input data

--- day-9/Program.cs
+++ day-9/Program.cs
@@ -6,17 +6,34 @@
         {
             // read in data
             string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
-            string data = File.ReadAllText(filepath);
+            string diskmap = File.ReadAllText(filepath);
 
-            Console.WriteLine(data);
+            Console.WriteLine(diskmap);
+
+            string data = String.Empty;
 
             // parse diskmap into a string
+            for (int i = 0, id = 0; i < diskmap.Length; i += 2, ++id)
             {
                 // diskmap is a string of pairs of digits
                 // 1st digit is file length
                 // 2nd digit is free space
                 // 12 = file length 1, free space 2 -> "0.."
+                
+                int filelength = int.Parse("" + diskmap[i]);
+                int freeSpace = 0;
+
+                if (i + 1 < diskmap.Length)
+                    freeSpace = int.Parse("" + diskmap[i + 1]);
+
+                for (int j = 0; j < filelength; ++j)
+                    data += "" + id;
+
+                for (int j = 0; j < freeSpace; ++j)
+                    data += ".";
             }
+
+            Console.WriteLine(data);
 
             // iterate through string
             {
