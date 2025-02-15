d354bb8697f616a731f57e57fe5066a86aa95e5a
megalon <megalongaming@gmail.com>
2025-02-15 13:30:26 -0500

    Psuedocode

--- day-12/Program.cs
+++ day-12/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // read in data
+
+            // convert string to char matrix array
+
+            // iterate through matrix
+            {
+                // when a valid character is found,
+                // begin searching for area
+                // probably use recursion
+                {
+
+                }
+            }
+        }
+
+        // return count and area
+        private static void Recursion(int x, int y, int area, int perimeter)
+        {
+            // if tile is not valid
+            {
+                // return count and 
+            }
+
+            // increment count
+
+            // switch(direction)
+            {
+                // case up
+                {
+                    // recurse right
+                    // recurse down
+                    // recurse left
+                }
+                // ...
+            }
+
+            // return the total count and the area
         }
     }
 }
