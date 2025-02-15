bfdef4eaaaf0315f3b5d33f0222fd112a30dbf5d
megalon <megalongaming@gmail.com>
2025-02-15 13:31:37 -0500

    Read data from file

--- day-12/Program.cs
+++ day-12/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             // read in data
+            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
+            string data = File.ReadAllText(filepath);
+
+            Console.WriteLine(data);
 
             // convert string to char matrix array
 
