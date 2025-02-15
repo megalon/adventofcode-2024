8cf7cc3ee7a9b23820c08e9f51a878c3b595e737
megalon <megalongaming@gmail.com>
2025-02-15 08:56:07 -0500

    notes and pseudocode

--- day-11/aoc-2024-day-11/Program.cs
+++ day-11/aoc-2024-day-11/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // read in data
+
+            // build a list of ulong
+
+            // loop 25 times
+            {
+                // check rules
+
+                // if stones[i] == 0
+                {
+                    // stones[i] = 1
+                    // continue;
+                }
+
+                // count digits
+
+                // if even number of digits
+                {
+                    // split in half
+                    // insert left half before this index
+                    // insert right half after this index
+
+                    // continue
+                }
+
+                // other rules didn't apply
+                // so multiply by 2024
+            }
+
+            // print stones count
         }
     }
 }
