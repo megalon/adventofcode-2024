981c67700e7c54e6624c9623908bad2965811cfa
megalon <megalongaming@gmail.com>
2025-02-15 09:55:28 -0500

    Break out algorithm into separate methods

--- day-11/aoc-2024-day-11/Program.cs
+++ day-11/aoc-2024-day-11/Program.cs
@@ -19,56 +19,70 @@ namespace aoc_2024_day_11
                 stones.Add(ulong.Parse(match.Value));
             }
 
-            // "blink" 25 times
-            for (int i = 0; i < 25; ++i)
+            Console.WriteLine("Part 1 total: " + BlinkNTimes(25, new List<ulong>(stones)));
+            Console.WriteLine("Part 2 total: " + BlinkNTimes(75, new List<ulong>(stones)));
+        }
+
+        private static int BlinkNTimes(int n, List<ulong> stones)
+        {
+            for (int i = 0; i < n; ++i)
             {
-                // iterate through stones list and check rules
-                for (int stoneIdx = 0; stoneIdx < stones.Count; ++stoneIdx)
-                {
-                    if (stones[stoneIdx] == 0)
-                    {
-                        stones[stoneIdx] = 1;
-                        continue;
-                    }
+                Console.WriteLine("Iteration: " + i);
 
-                    // Count digits
-                    int numDigits = CountDigits(stones[stoneIdx]);
-
-                    // if even number of digits
-                    if (numDigits % 2 == 0)
-                    {
-                        // split stone in half
-                        // left digits go to left stone
-                        // right digits go to right stone
-
-                        // ** This sort of thing would not work because of percision issues
-                        // Floor returns a double for example
-                        // Math.Floor(stones[stoneIdx] / Math.Pow(10, numDigits / 2));
-
-                        // Convert to a string and split so we can parse the string to a ulong
-                        string s = stones[stoneIdx].ToString();
-
-                        string left = s.Substring(0, numDigits / 2);
-                        string right = s.Substring(numDigits / 2);
-
-                        stones[stoneIdx] = ulong.Parse(left);
-                        stones.Insert(stoneIdx + 1, ulong.Parse(right));
-
-                        // Skip over the stone we just added
-                        ++stoneIdx;
-
-                        continue;
-                    }
-
-                    // other rules didn't apply
-                    // so multiply by 2024
-                    stones[stoneIdx] *= 2024;
-                }
-
-                Console.WriteLine(String.Join(' ', stones));
+                Blink(stones);
             }
 
-            Console.WriteLine("Total: " + stones.Count);
+            return stones.Count;
+        }
+
+        private static int Blink(List<ulong> stones)
+        {
+            // iterate through stones list and check rules
+            for (int stoneIdx = 0; stoneIdx < stones.Count; ++stoneIdx)
+            {
+                if (stones[stoneIdx] == 0)
+                {
+                    stones[stoneIdx] = 1;
+                    continue;
+                }
+
+                // Count digits
+                int numDigits = CountDigits(stones[stoneIdx]);
+
+                // if even number of digits
+                if (numDigits % 2 == 0)
+                {
+                    // split stone in half
+                    // left digits go to left stone
+                    // right digits go to right stone
+
+                    // ** This sort of thing would not work because of percision issues
+                    // Floor returns a double for example
+                    // Math.Floor(stones[stoneIdx] / Math.Pow(10, numDigits / 2));
+
+                    // Convert to a string and split so we can parse the string to a ulong
+                    string s = stones[stoneIdx].ToString();
+
+                    string left = s.Substring(0, numDigits / 2);
+                    string right = s.Substring(numDigits / 2);
+
+                    stones[stoneIdx] = ulong.Parse(left);
+                    stones.Insert(stoneIdx + 1, ulong.Parse(right));
+
+                    // Skip over the stone we just added
+                    ++stoneIdx;
+
+                    continue;
+                }
+
+                // other rules didn't apply
+                // so multiply by 2024
+                stones[stoneIdx] *= 2024;
+            }
+
+            //Console.WriteLine(String.Join(' ', stones));
+
+            return stones.Count;
         }
 
         private static int CountDigits(ulong number)
