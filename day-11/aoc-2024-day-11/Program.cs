a94f12917e3a73f5209cdf6aca2f15c393cca3ed
megalon <megalongaming@gmail.com>
2025-02-15 09:10:52 -0500

    Implement rules 1 and 3

--- day-11/aoc-2024-day-11/Program.cs
+++ day-11/aoc-2024-day-11/Program.cs
@@ -20,31 +20,47 @@ namespace aoc_2024_day_11
             }
 
             // loop 25 times
+            for (int i = 0; i < 25; ++i)
             {
-                // check rules
-
-                // if stones[i] == 0
+                // iterate through stones list and check rules
+                for (int stoneIdx = 0; stoneIdx < stones.Count; ++stoneIdx)
                 {
-                    // stones[i] = 1
-                    // continue;
+                    if (stones[stoneIdx] == 0)
+                    {
+                        stones[stoneIdx] = 1;
+                        continue;
+                    }
+
+                    // if even number of digits
+                    if (CountDigits(stones[stoneIdx]) % 2 == 0)
+                    {
+                        // split in half
+                        // insert left half before this index
+                        // insert right half after this index
+
+                        continue;
+                    }
+
+                    // other rules didn't apply
+                    // so multiply by 2024
+                    stones[stoneIdx] *= 2024;
                 }
 
-                // count digits
-
-                // if even number of digits
-                {
-                    // split in half
-                    // insert left half before this index
-                    // insert right half after this index
-
-                    // continue
-                }
-
-                // other rules didn't apply
-                // so multiply by 2024
+                Console.WriteLine();
             }
 
-            // print stones count
+            Console.WriteLine("Total: " + stones.Count);
+        }
+
+        private static int CountDigits(ulong number)
+        {
+            int digits = 1;
+            while ((number /= 10) > 0)
+            {
+                ++digits;
+            }
+
+            return digits;
         }
     }
 }
