c202e2fdf8a8b0199f193ac657056486a87e0302
megalon <megalongaming@gmail.com>
2025-02-16 09:07:31 -0500

    Notes and pseudocode

--- day-13/Program.cs
+++ day-13/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // read in data
+
+            // parse into some kind of collection
+
+            // iterate over claw machines
+            {
+                // iterate, adding each time
+                // max 100 iterations
+                {
+                    // do some fancy math to find the divisors
+                }
+
+                // If not possible to reach target, ignore
+
+                // calculate cost
+            }
+
+            // print total cost
         }
+
+        // class ClawMachine
+            // IVector2 buttonA
+            // IVector2 buttonB
+            // IVector2 target
+
+        // class IVector2
+            // x, y
     }
 }
