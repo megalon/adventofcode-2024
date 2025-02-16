1f0c7664152dba784456c76c143af2d099c489c6
megalon <megalongaming@gmail.com>
2025-02-16 10:36:09 -0500

    Just brute force it

--- day-13/Program.cs
+++ day-13/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics.Metrics;
+using System.Numerics;
 using System.Text.RegularExpressions;
 
 namespace aoc_2024_day_13
@@ -11,53 +12,55 @@ namespace aoc_2024_day_13
             string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
             string data = File.ReadAllText(filepath);
 
+            int totalCost = 0;
+
             foreach (Match match in Regex.Matches(data, @"(Button A: X\+\d+, Y\+\d+)\s+(Button B: X\+\d+, Y\+\d+)\s+(Prize: X=\d+, Y=\d+)"))
             {
                 IVector2 A = VectorFromInputString(match.Groups[1].Value);
                 IVector2 B = VectorFromInputString(match.Groups[2].Value);
                 IVector2 T = VectorFromInputString(match.Groups[3].Value);
 
-                // Loop maximums
-                // This is the max number of times we can add this
-                // number to get a number smaller than the target t
-                // It also has to be less than 100 by the puzzle rules
-                int maxA = (int)Math.Min(Math.Floor(T.x / (decimal)A.x), Math.Floor(T.y / (decimal)A.y));
-                int maxB = (int)Math.Min(Math.Floor(T.x / (decimal)B.x), Math.Floor(T.y / (decimal)B.y));
+                Console.WriteLine(T.ToString());
 
-                int maxCount = Math.Min(100, Math.Min(maxA, maxB));
+                totalCost += GetTokenCost(A, B, T);
 
-                IVector2 divisorA = new IVector2(0, 0);
-                IVector2 divisorB = new IVector2(0, 0);
-                int countInv = 0;
-
-                // iterate, adding each time
-                // max 100 iterations
-                for (int count = maxCount; count > 0; --count)
-                {
-                    divisorB = new IVector2(B.x * count, B.y * count);
-
-                    // if not divisible ignore
-                    if (T.x % divisorB.x != 0)
-                        continue;
-
-                    countInv = maxCount - count;
-
-                    divisorA = new IVector2(A.x * countInv, A.y * countInv);
-
-                    if (T.x % divisorA.x != 0)
-                        continue;
-
-                    // I guess that's it we found a match for x?
-                    Console.WriteLine("Match? Target: " + T.ToString());
-                    Console.WriteLine($"{T.x} * {count} + {T.y} * {countInv} = {(T.x * count) + (T.y * countInv)}");
-                }
-
-                // If not possible to reach target, ignore
-
-                // calculate cost
+                Console.WriteLine();
             }
 
             // print total cost
+            Console.WriteLine("Total cost: " +  totalCost);
+        }
+
+        private static int GetTokenCost(IVector2 A, IVector2 B, IVector2 T)
+        {
+            IVector2 result = new IVector2(0, 0);
+
+            // max 100 button presses
+            int maxCount = 100;
+            
+            // We want as few A presses as possible, so start at 0
+            // We want as many B presses as possible, so start at max count 
+            for (int countA = 0; countA < maxCount; ++countA)
+            {
+                for (int countB = maxCount; countB > 0; --countB)
+                {
+                    result = new IVector2(
+                        A.x * countA + B.x * countB,
+                        A.y * countA + B.y * countB
+                    );
+
+                    if (T == result)
+                    {
+                        Console.WriteLine("****** MATCH ******");
+                        //Console.WriteLine($"{result.ToString()} == {T.ToString()} -> {result == T}");
+                        Console.Write($"{A.ToString()} * {countA} + {B.ToString()} * {countB} = {result.ToString()}");
+
+                        return countA * 3 + countB;
+                    }
+                }
+            }
+         
+            return 0;
         }
 
         private static IVector2 VectorFromInputString(string input)
@@ -77,9 +80,17 @@ namespace aoc_2024_day_13
                 this.x = x;
                 this.y = y;
             }
+            public static bool operator ==(IVector2 left, IVector2 right)
+            {
+                return left.x == right.x && left.y == right.y;
+            }
+            public static bool operator !=(IVector2 left, IVector2 right)
+            {
+                return left.x != right.x || left.y != right.y;
+            }
             public override string ToString()
             {
-                return "" + x + ", " + y;
+                return "(" + x + ", " + y + ")";
             }
         }
     }
