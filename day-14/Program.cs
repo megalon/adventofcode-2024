a7f21b44eadd8dee585b0fb2c8f9b3d68dd24331
megalon <megalongaming@gmail.com>
2025-02-17 10:46:58 -0500

    Calculate safety factor

--- day-14/Program.cs
+++ day-14/Program.cs
@@ -10,6 +10,7 @@ namespace aoc_2024_day_14
             string text = File.ReadAllText(filepath);
 
             IVector2 mapSize = new IVector2(11, 7); // Test map
+            //IVector2 mapSize = new IVector2(101, 103);
             int numSeconds = 100;
 
             int[,] map = new int[mapSize.x, mapSize.y];
@@ -43,6 +44,51 @@ namespace aoc_2024_day_14
                 map[position.x, position.y] += 1;
             }
             PrintMap(map);
+
+            Console.WriteLine($"Part 1: {GetSafetyFactor(map)}");
+        }
+
+        private static uint GetSafetyFactor(int[,] map)
+        {
+            uint safetyFactor = 1;
+
+            int halfWidth = (int)Math.Floor((double)map.GetLength(0) / 2);
+            int halfHeight = (int)Math.Floor((double)map.GetLength(1) / 2);
+
+            IVector2 offset = IVector2.ZERO;
+
+            for (int quad = 0; quad < 4; ++quad)
+            {
+                switch (quad)
+                {
+                    case 1:
+                        offset = new IVector2(map.GetLength(0) - halfWidth, 0);
+                        break;
+                    case 2:
+                        offset = new IVector2(0, map.GetLength(1) - halfHeight);
+                        break;
+                    case 3:
+                        offset = new IVector2(map.GetLength(0) - halfWidth, map.GetLength(1) - halfHeight);
+                        break;
+                    default:
+                        offset = IVector2.ZERO;
+                        break;
+                }
+
+                int robotCount = 0;
+
+                for (int y = 0; y < halfHeight; ++y)
+                {
+                    for (int x = 0; x < halfWidth; ++x)
+                    {
+                        robotCount += map[x + offset.x, y + offset.y];
+                    }
+                }
+
+                safetyFactor *= (uint)robotCount;
+            }
+
+            return safetyFactor;
         }
 
         private static void PrintMap(int[,] map)
