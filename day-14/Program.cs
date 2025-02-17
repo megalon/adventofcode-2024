229fd3cd983c80d2b2aa4ba4c4ab85c55e0bfdb6
megalon <megalongaming@gmail.com>
2025-02-17 09:48:04 -0500

    Build char matrix with data

--- day-14/Program.cs
+++ day-14/Program.cs
@@ -9,12 +9,27 @@ namespace aoc_2024_day_14
             string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
             string text = File.ReadAllText(filepath);
 
+            IVector2 mapSize = new IVector2(11, 7); // Test map
+
+            int[,] map = new int[mapSize.x, mapSize.y];
+
             foreach (Match match in Regex.Matches(text, @"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)"))
             {
                 IVector2 position = new IVector2(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
                 IVector2 velocity = new IVector2(int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
 
                 Console.WriteLine($"p:{position.ToString()} v:{velocity.ToString()}");
+
+                map[position.x, position.y] += 1;
+            }
+
+            for (int y = 0; y < mapSize.y; ++y)
+            {
+                for (int x = 0; x < mapSize.x; ++x)
+                {
+                    Console.Write(map[x, y] == 0 ? "." : map[x, y]);
+                }
+                Console.WriteLine();
             }
         }
 
