1a247ac48d4b0de28b31fc8b92c2c6270ef87083
megalon <megalongaming@gmail.com>
2025-02-17 09:37:48 -0500

    parse input

--- day-14/Program.cs
+++ day-14/Program.cs
@@ -1,15 +1,35 @@
-﻿namespace aoc_2024_day_14
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_2024_day_14
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
-            string[] lines = File.ReadAllLines(filepath);
+            string text = File.ReadAllText(filepath);
 
-            foreach (string line in lines)
+            foreach (Match match in Regex.Matches(text, @"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)"))
             {
-                Console.WriteLine(line);
+                IVector2 position = new IVector2(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+                IVector2 velocity = new IVector2(int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+
+                Console.WriteLine($"p:{position.ToString()} v:{velocity.ToString()}");
+            }
+        }
+
+        public class IVector2
+        {
+            public int x { get; }
+            public int y { get; }
+            public IVector2(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public override string ToString()
+            {
+                return $"({x},{y})";
             }
         }
     }
