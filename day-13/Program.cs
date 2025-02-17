bf1d7bce29280e655d2994403c7103c6c1713607
megalon <megalongaming@gmail.com>
2025-02-16 19:36:16 -0500

    Use "Cramer's Rule" to solve part 2

--- day-13/Program.cs
+++ day-13/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.Metrics;
-using System.Numerics;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace aoc_2024_day_13
 {
@@ -17,48 +15,46 @@ namespace aoc_2024_day_13
 
             foreach (Match match in Regex.Matches(data, @"(Button A: X\+\d+, Y\+\d+)\s+(Button B: X\+\d+, Y\+\d+)\s+(Prize: X=\d+, Y=\d+)"))
             {
-                ULVector2 A = VectorFromInputString(match.Groups[1].Value);
-                ULVector2 B = VectorFromInputString(match.Groups[2].Value);
-                ULVector2 T = VectorFromInputString(match.Groups[3].Value);
+                BigVector2 A = VectorFromInputString(match.Groups[1].Value);
+                BigVector2 B = VectorFromInputString(match.Groups[2].Value);
+                BigVector2 T = VectorFromInputString(match.Groups[3].Value);
 
                 Console.WriteLine(T.ToString());
 
                 //totalCostPart1 += GetTokenCostPart1(A, B, T, 100);
-                totalCostPart1 += GetTokenCostPart2(A, B, T);
-                //totalCostPart2 += GetTokenCostPart2(A, B, new ULVector2(T.x + 10000000000000, T.y + 10000000000000));
+                totalCostPart2 += GetTokenCostPart2(A, B, new BigVector2(T.x + 10000000000000, T.y + 10000000000000));
 
                 Console.WriteLine();
             }
 
             Console.WriteLine("Total cost part 1: " + totalCostPart1);
+            Console.WriteLine("Total cost part 2: " + totalCostPart2);
         }
 
-        private static ulong GetTokenCostPart2(ULVector2 A, ULVector2 B, ULVector2 T)
+        private static ulong GetTokenCostPart2(BigVector2 A, BigVector2 B, BigVector2 T)
         {
-            // 80 * 94 + 40 * 22 = 8400
-            //      94 +      22 = 116
+            // "Cramer's Rule"
+            // det = (a_x * b_y - a_y * b_x)
+            // countA = (p_x * b_y - p_y * b_x) / det
+            // countB = (a_x * p_y - a_y * p_x) / det
 
-            // Max value 
+            double det = (A.x * B.y - A.y * B.x);
+            double countA = (T.x * B.y - T.y * B.x) / det;
+            double countB = (A.x * T.y - A.y * T.x) / det;
 
-            // The max number of button presses to check is
-            // how many presses it would take for the button
-            // that adds less to total up to a value greater than the target
-            decimal xMax = Math.Ceiling(Math.Max(T.x / A.x, T.x / B.x));
-            decimal yMax = Math.Ceiling(Math.Max(T.y / A.y, T.y / B.y));
+            // If either one is not a whole number,
+            // the input does not divide evenly and
+            // the claw machine is not winnable
+            // so return zero
+            if (countA % 1 > 0 || countB % 1 > 0)
+                return 0;
 
-            Part2Recursive(A, B, 0, 10000, T);
-
-            return 0;
+            return (ulong)(countA * 3 + countB);
         }
 
-        private static ulong Part2Recursive(ULVector2 A, ULVector2 B, decimal countA, decimal countB, ULVector2 T)
+        private static ulong GetTokenCostPart1(BigVector2 A, BigVector2 B, BigVector2 T, ulong maxPresses)
         {
-            return 0;
-        }
-
-        private static ulong GetTokenCostPart1(ULVector2 A, ULVector2 B, ULVector2 T, ulong maxPresses)
-        {
-            ULVector2 result = new ULVector2(0, 0);
+            BigVector2 result = new BigVector2(0, 0);
             
             // We want as few A presses as possible, so start at 0
             // We want as many B presses as possible, so start at max 
@@ -66,7 +62,7 @@ namespace aoc_2024_day_13
             {
                 for (ulong countB = maxPresses; countB > 0; --countB)
                 {
-                    result = new ULVector2(
+                    result = new BigVector2(
                         A.x * countA + B.x * countB,
                         A.y * countA + B.y * countB
                     );
@@ -84,28 +80,28 @@ namespace aoc_2024_day_13
             return 0;
         }
 
-        private static ULVector2 VectorFromInputString(string input)
+        private static BigVector2 VectorFromInputString(string input)
         {
             Match match = Regex.Match(input, @"\D+(\d+)\D+(\d+)");
 
-            return new ULVector2(decimal.Parse(match.Groups[1].Value), decimal.Parse(match.Groups[2].Value));
+            return new BigVector2(double.Parse(match.Groups[1].Value), double.Parse(match.Groups[2].Value));
         }
 
-        private struct ULVector2
+        private struct BigVector2
         {
-            public decimal x { get; }
-            public decimal y { get; }
+            public double x { get; }
+            public double y { get; }
 
-            public ULVector2(decimal x, decimal y)
+            public BigVector2(double x, double y)
             {
                 this.x = x;
                 this.y = y;
             }
-            public static bool operator ==(ULVector2 left, ULVector2 right)
+            public static bool operator ==(BigVector2 left, BigVector2 right)
             {
                 return left.x == right.x && left.y == right.y;
             }
-            public static bool operator !=(ULVector2 left, ULVector2 right)
+            public static bool operator !=(BigVector2 left, BigVector2 right)
             {
                 return left.x != right.x || left.y != right.y;
             }
