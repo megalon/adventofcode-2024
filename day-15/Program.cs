fc2de20d654e4642f297e8e0d6e537fac8ae49ea
megalon <megalongaming@gmail.com>
2025-02-18 08:38:56 -0500

    Parse input

--- day-15/Program.cs
+++ day-15/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace aoc_2024_day_15
 {
@@ -6,15 +7,35 @@ namespace aoc_2024_day_15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
             // read data
+            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt");
+            string text = File.ReadAllText(filepath);
 
-            // build char map
+            MatchCollection matches = Regex.Matches(text, @"#+\S+#\s");
+            IVector2 robot = new IVector2(0, 0);
 
-            // track robot position
+            // use char array so we can modify it when things move
+            char[,] map = new char[matches[0].Length, matches.Count];
+
+            for (int y = 0; y < matches.Count; ++y)
+            {
+                for (int x = 0; x < matches[0].Length; ++x)
+                {
+                    map[x, y] = matches[y].Value[x];
+
+                    if (map[x, y] == '@')
+                    {
+                        robot.x = x;
+                        robot.y = y;
+                    }
+                }
+            }
+
+            // Join all commands into single string
+            string commandsInput = string.Join("", Regex.Matches(text, @"[\^v<>]+").Select(x => x.Value).ToArray());
 
             // iterate through commands 
+            foreach (var command in commandsInput)
             {
                 // from robot position,
                 // check tile in direction of movemet
@@ -37,6 +58,17 @@ namespace aoc_2024_day_15
             }
 
             // print total
+        }
+    }
+
+    internal struct IVector2
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public IVector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
         }
     }
 }
