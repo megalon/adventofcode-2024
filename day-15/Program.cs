22a7b75e6356cadca4eb3a1dc6f30138d6176ef6
megalon <megalongaming@gmail.com>
2025-02-18 08:15:54 -0500

    pseudocode

--- day-15/Program.cs
+++ day-15/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace aoc_2024_day_15
+﻿using System.Runtime.CompilerServices;
+
+namespace aoc_2024_day_15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // read data
+
+            // build char map
+
+            // track robot position
+
+            // iterate through commands 
+            {
+                // from robot position,
+                // check tile in direction of movemet
+                // if it's a wall
+                //   continue
+                // if it's a box
+                //   check area in direction of movment + 1
+                // if it's empty space
+                //   move to that space, set the previous space to empty
+            }
+
+            // iterate through grid
+            {
+                // if this is not a box, ignore
+
+                // get the GPS coord
+                // x + (y * 100)
+
+                // add this to the total of all gps box positions
+            }
+
+            // print total
         }
     }
 }
